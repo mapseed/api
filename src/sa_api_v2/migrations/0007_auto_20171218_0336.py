c0fbdad614fe99a6bc4de31c0243b98b76a11a1c
modulitos <luke@lukeswart.net>
2017-12-17 19:40:04 -0800

    fix(sa_api_v2) rebase django 1.8 migration

--- src/sa_api_v2/migrations/0007_auto_20171218_0336.py
+++ src/sa_api_v2/migrations/0007_auto_20171218_0336.py
@@ -9,7 +9,7 @@ import sa_api_v2.models.profiles
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sa_api_v2', '0004_auto_20171027_0547'),
+        ('sa_api_v2', '0006_attachment_type'),
     ]
 
     operations = [
@@ -28,11 +28,6 @@ class Migration(migrations.Migration):
             model_name='placeemailtemplate',
             name='from_email',
             field=models.EmailField(max_length=254),
-        ),
-        migrations.AlterField(
-            model_name='placeemailtemplate',
-            name='submission_set',
-            field=models.CharField(help_text=b'Either the name of a submission set         (e.g., "comments"), or "places". Leave blank to         refer to all things.', max_length=128, blank=True),
         ),
         migrations.AlterField(
             model_name='user',
