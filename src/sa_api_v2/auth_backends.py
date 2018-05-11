c59580b3626e9e2b1b1c613f2af99e78b4cdcad5
goldpbear <tcroxson@gmail.com>
2018-05-11 08:37:31 -0700

    fix: don't send redirect_state query param in FB OAuth requests

--- src/sa_api_v2/auth_backends.py
+++ src/sa_api_v2/auth_backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from .cache import UserCache
+from social.backends.facebook import FacebookOAuth2
 
 class CachedModelBackend (ModelBackend):
     def get_user(self, user_id):
@@ -8,3 +9,6 @@ class CachedModelBackend (ModelBackend):
             user = super(CachedModelBackend, self).get_user(user_id)
             UserCache.set_instance(user, user_id=user_id)
         return user
+
+class NoRedirectStateFacebookOAuth2(FacebookOAuth2):
+    REDIRECT_STATE = False
