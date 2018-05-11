9da50bb5da0c04fb0a4597f7e613392d05d7d355
goldpbear <tcroxson@gmail.com>
2018-05-11 09:12:45 -0700

    add explanatory comment

--- src/sa_api_v2/auth_backends.py
+++ src/sa_api_v2/auth_backends.py
@@ -10,5 +10,10 @@ class CachedModelBackend (ModelBackend):
             UserCache.set_instance(user, user_id=user_id)
         return user
 
+# This custom backend prevents the dynamic redirect_state query param from
+# being sent with FB OAuth requests. redirect_state interferes with upgraded
+# FB security requirements.
+# https://stackoverflow.com/questions/45307723/valid-oauth-redirect-uris-for-facebook-django-social-auth
+# https://developers.facebook.com/docs/facebook-login/security/#surfacearea
 class NoRedirectStateFacebookOAuth2(FacebookOAuth2):
     REDIRECT_STATE = False
