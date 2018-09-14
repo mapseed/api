6e4d2b8670725ddcc1aaa14f33d35486f96f7a00
jalmogo <you@example.com>
2018-09-14 01:43:21 -0700

    feat(SubmissionListView): enable email notifications for comments

--- src/sa_api_v2/views/base_views.py
+++ src/sa_api_v2/views/base_views.py
@@ -797,6 +797,118 @@ class CachedResourceMixin (object):
         return response
 
 
+# TODO: A class/mixin isn't needed. Refactor this into a function.
+class EmailTemplateMixin(object):
+    def send_email_notification(self, obj, submission_set_name):
+        # TODO: until we establish a many:one relationship between the
+        # Origin and EmailTemplate models, we are temporarily
+        # disabling email notifications for Supports. Otherwise, there
+        # is no way to have an EmailTemplate trigger on only Place and
+        # Comment submissions.
+        if submission_set_name == 'support':
+            return
+
+        request_origin = self.request.META.get('HTTP_ORIGIN', '')
+        email_templates = [origin.place_email_template for origin in obj.dataset.origins.all()
+                           if cors.models.Origin.match(origin.pattern, request_origin) and
+                           origin.place_email_template is not None]
+
+        filtered_email_templates = filter(
+            lambda template: template.submission_set in [submission_set_name, ''] and
+            template.event == 'add',
+            email_templates
+        )
+
+        for email_template in filtered_email_templates:
+            logger.info('[EMAIL] Starting email send')
+
+            from_email = email_template.from_email
+            bcc_sources = [email_template.bcc_email_1,
+                           email_template.bcc_email_2,
+                           email_template.bcc_email_3,
+                           email_template.bcc_email_4,
+                           email_template.bcc_email_5]
+            bcc_list = [source for source in bcc_sources if source]
+
+            logger.debug('[EMAIL] Got from email')
+
+            errors = []
+
+            try:
+                email_field = email_template.recipient_email_field
+                recipient_email = self.request.DATA[email_field]
+                logger.debug('[EMAIL] recipient_email: ' + recipient_email)
+            except KeyError:
+                logger.debug('[EMAIL] No primary recipient found. Setting primary recipient to the empty string.')
+                recipient_email = ""
+
+            logger.debug('[EMAIL] Got to email')
+
+            # If the user didn't provide an email address, and no BCC emails are provided,
+            # then we can't send an email
+            if not recipient_email and not bcc_list:
+                errors.append('[EMAIL] Error: No primary recipient and no BCC recipients provided. Email will not be sent.')
+
+            # Bail if any errors were found. Send all errors to the logs and otherwise
+            # fail silently.
+            if errors:
+                for error_msg in errors:
+                    logger.error(error_msg)
+                continue
+
+            # If we didn't find any errors, then render the email and send.
+            logger.debug('[EMAIL] Going ahead, no errors')
+
+            # TODO: if this is a comment notification, then add a
+            # `comment` key to the context as well. Note that we'll
+            # have to change the Origin:EmailTemplate to be One:Many
+            # for this to work.
+            context_data = RequestContext(self.request, {
+                'place': obj,
+                'email': recipient_email
+            })
+
+            logger.debug('[EMAIL] Got context data')
+
+            subject = Template(email_template.subject).render(context_data)
+            body = Template(email_template.body_text).render(context_data)
+
+            logger.debug('[EMAIL] Rendered text')
+
+            if email_template.body_html:
+                html_body = Template(email_template.body_html).render(context_data)
+                logger.debug('[EMAIL] Rendered html')
+            else:
+                html_body = None
+
+            # connection = smtp.EmailBackend(
+            #     host=...,
+            #     port=...,
+            #     username=...,
+            #     use_tls=...)
+
+            # NOTE: In Django 1.7+, send_mail can handle multi-part email with the
+            # html_message parameter, but pre 1.7 cannot and we must construct the
+            # multipart message manually.
+            msg = EmailMultiAlternatives(
+                subject,
+                body,
+                from_email,
+                to=[recipient_email],
+                bcc=bcc_list)
+            # connection=connection)
+
+            logger.debug('[EMAIL] Created email')
+
+            if html_body:
+                msg.attach_alternative(html_body, 'text/html')
+                logger.debug('[EMAIL] Attached html')
+
+            msg.send()
+            logger.info('[EMAIL] %s email for %d sent.', submission_set_name, obj.id)
+            break
+
+
 class Sanitizer(object):
     """
     Strip out non-whitelisted HTML tags and attributes in an object of submitted data.
@@ -1023,7 +1135,7 @@ class PlaceListMixin (object):
     pass
 
 
-class PlaceListView (Sanitizer, CachedResourceMixin, LocatedResourceMixin, OwnedResourceMixin, FilteredResourceMixin, bulk_generics.ListCreateBulkUpdateAPIView):
+class PlaceListView (Sanitizer, CachedResourceMixin, LocatedResourceMixin, OwnedResourceMixin, FilteredResourceMixin, EmailTemplateMixin, bulk_generics.ListCreateBulkUpdateAPIView):
     """
 
     GET
@@ -1135,18 +1247,7 @@ class PlaceListView (Sanitizer, CachedResourceMixin, LocatedResourceMixin, Owned
         if len(webhooks):
             self.trigger_webhooks(webhooks, obj)
 
-        origin = self.request.META.get('HTTP_ORIGIN', '')
-        email_templates = [o.place_email_template for o in obj.dataset.origins.all()
-                           if cors.models.Origin.match(o.pattern, origin) and
-                           o.place_email_template is not None]
-
-        email_templates = filter(
-            lambda et: et.submission_set in ['places', ''] and et.event == 'add',
-            email_templates
-        )
-
-        if len(email_templates):
-            self.trigger_emails(email_templates, obj)
+        self.send_email_notification(obj, submission_set_name='places')
 
     def get_queryset(self):
         dataset = self.get_dataset()
@@ -1212,108 +1313,20 @@ class PlaceListView (Sanitizer, CachedResourceMixin, LocatedResourceMixin, Owned
         renderer = renderers.GeoJSONRenderer()
         data = renderer.render(serializer.data)
 
-        # POST to each webhoo
+        # POST to each webhook
         for webhook in webhooks:
             status_code = 'None'
             try:
                 response = requests.post(webhook.url, data=data)
                 status_code = str(response.status_code)
                 response.raise_for_status()
-                logger.info('[WEBHOOK] Place %d added and POSTed to %s. Status: %s', obj.id,
-                    webhook.url, status_code)
+                logger.info('[WEBHOOK] Place %d added and POSTed to %s. Status: %s',
+                            obj.id, webhook.url, status_code)
             except requests.exceptions.RequestException as e:
                 logger.error('[WEBHOOK] Place %d added but could not be POSTed to %s. Status: %s',
-                    obj.id, webhook.url, status_code)
+                             obj.id, webhook.url, status_code)
                 logger.error(e)
 
-    def trigger_emails(self, email_templates, obj):
-        """
-        Sends emails based on the origin.
-        """
-        for email_template in email_templates:
-            logger.info('[EMAIL] Starting email send')
-
-            from_email = email_template.from_email
-            bcc_sources = [email_template.bcc_email_1,
-                           email_template.bcc_email_2,
-                           email_template.bcc_email_3,
-                           email_template.bcc_email_4,
-                           email_template.bcc_email_5]
-            bcc_list = [source for source in bcc_sources if source]
-
-            logger.debug('[EMAIL] Got from email')
-
-            errors = []
-
-            try:
-                email_field = email_template.recipient_email_field
-                recipient_email = self.request.DATA[email_field]
-                logger.debug('[EMAIL] recipient_email: ' + recipient_email)
-            except KeyError:
-                logger.debug('[EMAIL] No primary recipient found. Setting primary recipient to the empty string.')
-                recipient_email = ""        
-
-            logger.debug('[EMAIL] Got to email')
-
-            # If the user didn't provide an email address, and no BCC emails are provided,
-            # then we can't send an email
-            if not recipient_email and not bcc_list:
-                errors.append('[EMAIL] Error: No primary recipient and no BCC recipients provided. Email will not be sent.')
-
-            # Bail if any errors were found. Send all errors to the logs and otherwise
-            # fail silently.
-            if errors:
-                for error_msg in errors:
-                    logger.error(error_msg)
-                return
-
-            logger.debug('[EMAIL] Going ahead, no errors')
-
-            # If we didn't find any errors, then render the email and send.
-            context_data = RequestContext(self.request, {
-                'place': obj,
-                'email': recipient_email
-            })
-
-            logger.debug('[EMAIL] Got context data')
-
-            subject = Template(email_template.subject).render(context_data)
-            body = Template(email_template.body_text).render(context_data)
-
-            logger.debug('[EMAIL] Rendered text')
-
-            if email_template.body_html:
-                html_body = Template(email_template.body_html).render(context_data)
-                logger.debug('[EMAIL] Rendered html')
-            else:
-                html_body = None
-
-            # connection = smtp.EmailBackend(
-            #     host=...,
-            #     port=...,
-            #     username=...,
-            #     use_tls=...)
-
-            # NOTE: In Django 1.7+, send_mail can handle multi-part email with the
-            # html_message parameter, but pre 1.7 cannot and we must construct the
-            # multipart message manually.
-            msg = EmailMultiAlternatives(
-                subject,
-                body,
-                from_email,
-                to=[recipient_email],
-                bcc=bcc_list)
-            # connection=connection)
-
-            logger.debug('[EMAIL] Created email')
-
-            if html_body:
-                msg.attach_alternative(html_body, 'text/html')
-                logger.debug('[EMAIL] Attached html')
-
-            msg.send()
-            logger.info('[EMAIL] Email for place %d sent.', obj.id)
-            break
 
 class SubmissionInstanceView (CachedResourceMixin, OwnedResourceMixin, generics.RetrieveUpdateDestroyAPIView):
     """
@@ -1378,7 +1391,7 @@ class SubmissionInstanceView (CachedResourceMixin, OwnedResourceMixin, generics.
         return obj
 
 
-class SubmissionListView (CachedResourceMixin, OwnedResourceMixin, FilteredResourceMixin, bulk_generics.ListCreateBulkUpdateAPIView):
+class SubmissionListView (CachedResourceMixin, OwnedResourceMixin, FilteredResourceMixin, EmailTemplateMixin, bulk_generics.ListCreateBulkUpdateAPIView):
     """
 
     GET
@@ -1440,6 +1453,9 @@ class SubmissionListView (CachedResourceMixin, OwnedResourceMixin, FilteredResou
         obj.dataset = self.get_dataset()
         obj.place = self.get_place(obj.dataset)
         obj.set_name = self.kwargs[self.submission_set_name_kwarg]
+
+    def post_save(self, obj, created):
+        self.send_email_notification(obj, submission_set_name=obj.set_name)
 
     def get_queryset(self):
         dataset = self.get_dataset()
